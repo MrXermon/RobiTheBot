ae48afea4d9d2911014efc2c83f12d93408ec716
MrXermon <jan@jangilla.de>
2019-12-05 17:28:39 +0000

    Counter for turns

--- WS19-OptRob-T2-Ch2.py
+++ WS19-OptRob-T2-Ch2.py
@@ -11,23 +11,34 @@ Bot.speed(40)
 IRSensor = TRSensor()
 
 try:
+	count = 1
 	# Loop until forever
 	while True:
 		Bot.forward()
 		# Detect black line
-		if IRSensor.AnalogRead()[3] <= 200:
+		if IRSensor.AnalogRead()[3] <= 800:
+			print "Linie erkannt!"
 			# Stop and wait
 			Bot.stop()
-			time.sleep(1)
+			time.sleep(0.5)
 
 			# Drive backwards
 			Bot.backward()
+			time.sleep(0.5)
+			Bot.stop()
+
+			# Turn direction
+			if count % 5 == 0:
+				Bot.right()
+			else:
+				Bot.left()
+
+			# Wait for turn
 			time.sleep(1)
 			Bot.stop()
 
-			# Turn right
-			Bot.right()
-			time.sleep(2)
-			Bot.stop()
+			# Increment counter
+			count = count + 1
+
 except KeyboardInterrupt:
 	GPIO.cleanup()
